c2c28c20612cd351bead77673c7301c57fc6a908
JokinenL <lauri.a.i.jokinen@gmail.com>
2023-09-19 18:04:13 +0300

    Alkeellinen sinisen sivun tunnistaminen

--- cube_solver.py
+++ cube_solver.py
@@ -1,5 +1,6 @@
 import cv2 as cv2
 import kociemba as cube
+import numpy as np
 
 # initial_state = input("Enter the initial state of the cube: ")
 
@@ -10,10 +11,32 @@ import kociemba as cube
 video = cv2.VideoCapture(0)
 while True:
 
-    is_ok, image_input = video.read()
-    cv2.imshow("output", image_input)
+    is_ok, input_image = video.read()
+
+    hsv = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([90, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    result = cv2.bitwise_and(input_image, input_image, mask=mask)
+
+    cv2.imshow('input_image', input_image )
+    cv2.imshow('result', result)
+    cv2.imshow('mask', mask)
+
+
+
+    
 
     if cv2.waitKey(1) == ord('q'):
+        break
+    if cv2.waitKey(1) == ord('p'):
+        # TODO: Tulosta input_imagen tiedot:
+        # Kuinka suuri taulukko kyseessä?
+        # Yksittäisten piksleien väri?
+        # Tätä kautta saat kalibbroitua värientunnistusrajat optimaalisiksi
+        # juuri kyseiseen ympäristöön nähden.
         break
 
 video.release()
