115a328094b507f433442f7d359127e44422bb83
JokinenL <lauri.a.i.jokinen@gmail.com>
2023-09-24 14:56:07 +0300

    Värien HSV-arvojen rajat määritetään manuaalisesti

--- cube_solver.py
+++ cube_solver.py
@@ -7,37 +7,135 @@ import numpy as np
 # solution = cube.solve(initial_state)
 # print("Solution is:")
 # print(solution)
+width = 640
+height = 360
+center_w = width//2
+center_h = height//2
 
-video = cv2.VideoCapture(0)
-while True:
+def my_print(x, str):
+    print(str,end=": ")
+    print(x)
+    return
 
-    is_ok, input_image = video.read()
+def print_color_info(color_name, img):
+    BGR_color = np.uint8([[img[center_h][center_w]]])
+    HSV_color = cv2.cvtColor((BGR_color), cv2.COLOR_BGR2HSV)
+    # print(color_name + " (BGR): ",end="")
+    # print(BGR_color)
+    print(color_name + " (HSV): ",end="")
+    print(HSV_color)
+    print()
 
-    hsv = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([90, 50, 50])
-    upper_blue = np.array([130, 255, 255])
+def get_sigle_color_limits(img):
 
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    BGR_color = np.uint8([[img[center_h][center_w]]])
+    HSV_color = cv2.cvtColor((BGR_color), cv2.COLOR_BGR2HSV)
+    hue = HSV_color[0][0][0]
 
-    result = cv2.bitwise_and(input_image, input_image, mask=mask)
+    hue_low = hue - 2
+    hue_high = hue + 2
 
-    cv2.imshow('input_image', input_image )
-    cv2.imshow('result', result)
-    cv2.imshow('mask', mask)
-
-
-
+    if hue_low < 0:
+        hue_low = 0
+        hue_high = 4
     
+    if hue_high > 180:
+        hue_high = 180
+        hue_low = 176
 
-    if cv2.waitKey(1) == ord('q'):
-        break
-    if cv2.waitKey(1) == ord('p'):
-        # TODO: Tulosta input_imagen tiedot:
-        # Kuinka suuri taulukko kyseessä?
-        # Yksittäisten piksleien väri?
-        # Tätä kautta saat kalibbroitua värientunnistusrajat optimaalisiksi
-        # juuri kyseiseen ympäristöön nähden.
-        break
+    low = [hue_low, 100, 100]
+    high = [hue_high, 255, 255]
+    limits = [low, high]
+    return limits
 
-video.release()
-cv2.destroyAllWindows()
+def calibration_completed(hsv_limits):
+    for color in list(hsv_limits.keys()):
+        if hsv_limits[color][1][2] == 0:
+            return False
+
+    return True
+
+def main():
+
+    video = cv2.VideoCapture(0)
+    color = (255,0,0)
+
+    low = [0,0,0]
+    high = [0,0,0]
+
+    hsv_limits = {"blue": [low,high], "green": [low,high], "yellow": [low,high],
+                  "white": [low,high], "red": [low,high], "orange": [low,high]}
+    while True:
+
+
+        is_ok, img = video.read()
+
+        if not is_ok:
+            break
+
+        img = cv2.resize(img, (width, height))
+        img = cv2.flip(img, 1)
+        
+        
+
+        cv2.rectangle(img, (width//2-100, height//2-100), (width//2+100, height//2+100), (255, 0, 0), 5)
+        cv2.rectangle(img, (center_w-5, center_h-5), (center_w+5, center_h+5), (255, 0, 0), 3)
+        cv2.rectangle(img, (10,10), (30,30), color, 20)
+        cv2.imshow('img', img)
+        # cv2.imshow('hsv', HSV_img)
+
+        pressed = cv2.waitKey(1)
+
+        if pressed == ord('q'):
+            break
+
+        if pressed == ord('p'):
+            print(hsv_limits)
+
+        if pressed == ord('d'):
+            BGR_color = img[center_h][center_w]
+            B = BGR_color[0]
+            G = BGR_color[1]
+            R = BGR_color[2]
+
+            color = tuple([int(B),int(G),int(R)])
+            print("From the if statement: ", end="")
+            print(color)
+
+        if pressed == ord('b'):
+            print_color_info("Blue", img)
+            hsv_limits["blue"] = get_sigle_color_limits(img)
+            
+        if pressed == ord('g'):
+            print_color_info("Green", img)
+            hsv_limits["green"] = get_sigle_color_limits(img)
+
+        if pressed == ord('y'):
+            print_color_info("Yellow", img)
+            hsv_limits["yellow"] = get_sigle_color_limits(img)
+
+
+        if pressed == ord('w'):
+            print_color_info("White", img)
+            hsv_limits["white"] = get_sigle_color_limits(img)
+
+
+        if pressed == ord('r'):
+            print_color_info("Red", img)
+            hsv_limits["red"] = get_sigle_color_limits(img)
+
+        if pressed == ord('o'):
+            print_color_info("Orange", img)
+            hsv_limits["orange"] = get_sigle_color_limits(img)
+
+        if pressed == ord('a'):
+            if calibration_completed(hsv_limits):
+                print(hsv_limits)
+            else:
+                print("Please finnish the calibration")
+
+    video.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
